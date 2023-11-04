cc66baea6247ff6d7d7c8f5828a217c7d31c5651
Kimutai <jankimsingoei@gmail.com>
2023-11-04 15:41:58 +0300

    Progress

--- server/app.py
+++ server/app.py
@@ -1,13 +1,27 @@
 from flask_restful import Resource
 from config import app,api,bcrypt
 from models import db,User
-from flask import make_response,jsonify,request
+from flask import make_response,jsonify,request,session
 
+@app.before_request
+def check_if_logged_in():
+    if not session.get("user_id") and request.endpoint not in ["login", "checksession"]:
+        return make_response(jsonify({"error": "Unauthorized"}), 401)
 class Home(Resource):
     def get(self):
         response = make_response(jsonify({"message":"Welcome to BlogBloom API"}))
         return response
 api.add_resource(Home,"/")
+class CheckSession(Resource):
+    def get(self):
+        if session.get('user_id'):
+            user = User.query.filter(User.id==session["user_id"]).first()
+            response = make_response(jsonify({
+                "user_id":user.id
+            }),200)
+            return response
+        return {"error":"Resource not found"},404
+api.add_resource(CheckSession,'/checksession',endpoint='checksession')
 class RegistrationResource(Resource):
     def post(self):
         data = request.get_json()
@@ -21,25 +35,38 @@ class RegistrationResource(Resource):
         if user_exists:
             return jsonify({
                 "error":"User already exists",
-                "user":user_exists
             })
         user = User(username=username,email=email,_password_hash=hashed_password)
+        session['user_id']=user.id
         db.session.add(user)
         db.session.commit()
         response = make_response(jsonify({"message":"Registration successful"}),201)
         return response
-api.add_resource(RegistrationResource,"/registration")
+api.add_resource(RegistrationResource,"/registration",endpoint='registration')
 
 class LoginResource(Resource):
     def post(self):
         data = request.get_json()
-        email = data["email"]
-        password = data["password"]
-        user = User.query.filter(User.email == email).first()
-        if user and user.authenticate(password):
-            return make_response(jsonify({"message": "Login successful"}),201)
-        return make_response(jsonify({"error":"Incorrect password"}),401)
-api.add_resource(LoginResource,"/login")
+        email = data.get("email")
+        password = data.get("password")
+        user = User.query.filter_by(email=email).first()
 
+        if user:
+            if user.authenticate(password):
+                session['user_id'] = user.id
+                return make_response(jsonify({"message": "Login successful"}), 201)
+            else:
+                return make_response(jsonify({"error": "Incorrect password"}), 401)
+        else:
+            return make_response(jsonify({"error": "User not found"}), 404)
+api.add_resource(LoginResource, '/login', endpoint='login')
+class Logout(Resource):
+    def delete(self):
+        if session.get("user_id"):
+            session['user_id']=None
+            return {"success":"you have been logged out successfully"}
+        else:
+            return {"error":"unauthorized 401"}
+api.add_resource(Logout,'/logout',endpoint='logout')
 if __name__ == "__main__":
     app.run(debug=True,port=5555)
